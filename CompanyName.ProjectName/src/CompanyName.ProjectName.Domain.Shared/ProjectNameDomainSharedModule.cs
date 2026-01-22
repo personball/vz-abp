33ee0427f15f65c39dab2f976bc5d5d75d7c3438
personball <personball@163.com>
2026-01-22 16:40:31 +0800

    utc 时间问题注释

--- CompanyName.ProjectName/src/CompanyName.ProjectName.Domain.Shared/ProjectNameDomainSharedModule.cs
+++ CompanyName.ProjectName/src/CompanyName.ProjectName.Domain.Shared/ProjectNameDomainSharedModule.cs
@@ -41,6 +41,20 @@ public class ProjectNameDomainSharedModule : AbpModule
         {
             options.Kind = DateTimeKind.Utc;
         });
+        /*
+            {
+                "clockNow": "2026-01-22T08:34:58.8257187Z",
+                "dateTimeNow": "2026-01-22T08:34:58.825719Z",
+                "dateTimeOffsetNow": "2026-01-22T16:34:58.8257206+08:00",
+                "dateTimeNowStr": "2026-01-22 16:34:58",
+                "dateTimeUtcNowStr": "2026-01-22 08:34:58",
+                "dateTimeOffsetNowStr": "2026-01-22 16:34:58 +08:00",
+                "dateTimeOffsetUtcNowStr": "2026-01-22 08:34:58 +00:00"
+            }
+            在进程空间内，DateTime.Now 和 DateTimeOffset.Now 依然是服务端时区，东八区
+            直接以时间类型输出webapi时，会被abp框架处理为UTC时间
+            存到pg里时，也会被处理为UTC时间
+        */
 
         Configure<AbpVirtualFileSystemOptions>(options =>
         {
